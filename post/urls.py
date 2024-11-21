ae2a9190edca0211d7493a2e529bcdc86f876f9f
allan zizinga <allanzizinga@yahoo.com>
2024-11-22 00:10:40 +0100

    Added all urls for the pages and linked them together

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="home"),
-    path('nutrition/<str:pk>/',views.nutrition, name='nutrition'),
+    path('nutrition/',views.nutrition, name='nutrition'),
+    path('sports_tips/',views.sports_tips, name='sports_tips'),
+    path('insights/',views.insights, name='insights'),
+    path('health/',views.health, name='health'),
+    
     
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
@@ -7,6 +8,28 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'post/index.html')
 
+
 def nutrition(request):
-    return render(request, 'nutrition/nutrition.html')
+    nutrition_posts=Post.objects.filter(title__icontains='nutrition')
+    # Use 'icontains' for case-insensitive matching
+    context ={'nutrition_posts':nutrition_posts}
+    return render(request, 'post/nutrition.html', 
+         context
+    )
+
+def sports_tips(request):
+    tips=Post.objects.filter(title__icontains='tips')
+    context={'tips':tips}
+    return render(request,'post/sports_tips.html',context)
+
+def insights(request):
+    insights=Post.objects.filter(title__icontains='insights')
+    context={'insights':insights}
+    return render(request, 'post/insights.html', context)
+
+def health(request):
+    health_tips=Post.objects.filter(title__icontains='health')
+    context={'health_tips': health_tips}
+    return render(request,'post/health.html', context)
+
 
